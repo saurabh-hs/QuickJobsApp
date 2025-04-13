6b5d4917475e529da9b12d2d9619069004c738b2
saurabh-hs <shshinde@mitaoe.ac.in>
2025-04-13 15:13:16 +0530

    Profile page in-progress

--- quickjobs-frontend/src/Profile/Profile.tsx
+++ quickjobs-frontend/src/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Divider } from "@mantine/core";
+import { ActionIcon, Divider, TagsInput, Textarea } from "@mantine/core";
 import avatar from "../assets/profileavatar.png";
 import { IconBriefcase, IconDeviceFloppy, IconMapPin, IconPencil } from "@tabler/icons-react";
 import ExpCard from "./ExpCard";
@@ -10,6 +10,8 @@ import SelectInput from "./SelectInput";
 const Profile=(props:any)=> {
     const select=fields;
     const [edit, setEdit]=useState([false, false, false, false, false])
+    const [about, setAbout]=useState(`${props.about}`);
+    const [skills, setSkills]=useState(["HTML","CSS","JavaScript","React","Angular","Node.js","Python","Java","Ruby","PHP","SQL","MongoDB","PostgreSQL","Git","API Development","Testing and Debugging","Agile Methodologies","DevOps","AWS","Azure","Google Cloud"]);
     const handleEdit=(index:any)=>{
         const newEdit=[...edit];
         newEdit[index]=!newEdit[index];
@@ -39,21 +41,31 @@ const Profile=(props:any)=> {
         </div>
         <Divider mx="xs" my="xl"/>
         <div className="px-3">
-            <div className="text-2xl font-semibold mb-3 flex flex-start text-cloud-burst-700">About</div>
-            <div className="text-sm text-cloud-burst-300 text-justify text-cloud-burst-400">{props.about}</div>
+            <div className="text-2xl font-semibold mb-3 flex flex-start text-cloud-burst-700 justify-between">About <ActionIcon onClick={()=>handleEdit(1)} size="lg" color="cloud-burst.9" variant="subtle">
+                {edit[1]?<IconDeviceFloppy className="h-4/5 w-4/5" />:<IconPencil className="h-4/5 w-4/5" />}
+            </ActionIcon></div>
+            {
+                edit[1] ?   <Textarea value={about} placeholder="Enter about yourself..." autosize minRows={3} onChange={(event) => setAbout(event.currentTarget.value)} /> : <div className="text-sm text-cloud-burst-300 text-justify text-cloud-burst-400">{about}</div>
+            }
         </div>
         <Divider mx="xs" my="xl"/>
         <div className="px-3">
-            <div className="text-2xl font-semibold mb-3 flex flex-start text-cloud-burst-700">Skills</div>
-            <div className="flex flex-wrap gap-2">
+            <div className="text-2xl font-semibold mb-3 flex flex-start text-cloud-burst-700 justify-between">Skills <ActionIcon onClick={()=>handleEdit(2)} size="lg" color="cloud-burst.9" variant="subtle">
+                {edit[2]?<IconDeviceFloppy className="h-4/5 w-4/5" />:<IconPencil className="h-4/5 w-4/5" />}
+            </ActionIcon></div>
+            {
+                edit[2] ? <TagsInput value={skills} onChange={setSkills} placeholder="Add Skill" splitChars={[',',' ','|']} /> : <div className="flex flex-wrap gap-2">
                 {
                     props.skills.map((skill:any, index:any)=> <div key={index} className="bg-cloud-burst-900 text-sm font-medium bg-opacity-80 rounded-3xl text-cloud-burst-50 px-3 py-1">{skill}</div>)
                 }
             </div>
+            }
         </div>
         <Divider mx="xs" my="xl"/>
         <div className="px-3">
-            <div className="text-2xl font-semibold mb-5 flex flex-start text-cloud-burst-700">Experience</div>
+            <div className="text-2xl font-semibold mb-5 flex flex-start text-cloud-burst-700 justify-between">Experience <ActionIcon onClick={()=>handleEdit(3)} size="lg" color="cloud-burst.9" variant="subtle">
+                {edit[3]?<IconDeviceFloppy className="h-4/5 w-4/5" />:<IconPencil className="h-4/5 w-4/5" />}
+            </ActionIcon></div>
             <div className="flex flex-col gap-8">
                 {
                     props.experience.map((exp:any, index:any) =>  <ExpCard key={index} {...exp} />)
@@ -62,7 +74,9 @@ const Profile=(props:any)=> {
         </div>
         <Divider mx="xs" my="xl"/>
         <div className="px-3">
-            <div className="text-2xl font-semibold mb-5 flex flex-start text-cloud-burst-700">Certifications</div>
+            <div className="text-2xl font-semibold mb-5 flex flex-start text-cloud-burst-700 justify-between">Certifications <ActionIcon onClick={()=>handleEdit(4)} size="lg" color="cloud-burst.9" variant="subtle">
+                {edit[4]?<IconDeviceFloppy className="h-4/5 w-4/5" />:<IconPencil className="h-4/5 w-4/5" />}
+            </ActionIcon></div>
             <div className="flex flex-col gap-8">
                 {
                     props.certifications.map((cert:any, index:any) =>  <CertCard key={index} {...cert} />)
