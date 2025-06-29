2c446cc6d05a4bc1325bafd7387d698ba74a5248
saurabh-hs <shshinde@mitaoe.ac.in>
2025-06-29 21:22:22 +0530

    Change profile is in progress

--- quickjobs-frontend/src/Profile/Profile.tsx
+++ quickjobs-frontend/src/Profile/Profile.tsx
@@ -1,16 +1,20 @@
 import { ActionIcon, Divider, TagsInput, Textarea } from "@mantine/core";
 import avatar from "../assets/profileavatar.png";
-import { IconBriefcase, IconDeviceFloppy, IconMapPin, IconPencil, IconPlus } from "@tabler/icons-react";
+import {IconDeviceFloppy, IconMapPin, IconPlus } from "@tabler/icons-react";
 import ExpCard from "./ExpCard";
 import CertCard from "./CertCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import fields from "../Data/Profile";
-import SelectInput from "./SelectInput";
 import ExpInput from "./ExpInput";
 import CertInput from "./CertInput";
+import { useSelector } from "react-redux";
+import { getProfile } from "../Services/ProfileService";
+import {Info} from "../Profile/Info";
 
 const Profile=(props:any)=> {
     const select=fields;
+    const user = useSelector((state:any)=>state.user);
+    const profile = useSelector((state:any)=>state.profile);
     const [edit, setEdit]=useState([false, false, false, false, false])
     const [addExp, setAddExp] = useState(false);
     const [addCert, setAddCert] = useState(false);
@@ -21,28 +25,21 @@ const Profile=(props:any)=> {
         newEdit[index]=!newEdit[index];
         setEdit(newEdit);
     }
+
+    useEffect(()=>{
+        getProfile(user.id).then((data:any)=>{
+            console.log(data);
+        }).catch((error:any)=>{
+            console.log(error);
+        });
+    }, [])
     return <div className="w-4/5 mx-auto">
         <div className="relative">
             <img className="rounded-t-2xl" src="../../public/Profile/banner.jpg" alt="" />
             <img className="w-48 h-48 rounded-full -bottom-1/3 absolute left-3 border-cloud-burst-50 border-8" src={avatar} alt="" />
         </div>
         <div className="px-3 mt-16">
-            <div className="text-3xl font-semibold flex justify-between text-cloud-burst-700">{props.name} <ActionIcon onClick={()=>handleEdit(0)} size="lg" color="cloud-burst.9" variant="subtle">
-                {edit[0]?<IconDeviceFloppy className="h-4/5 w-4/5" />:<IconPencil className="h-4/5 w-4/5" />}
-            </ActionIcon> </div>
-            {
-                edit[0]?<><div className="flex gap-10 [&>*]:w-1/2 text-left">
-                <SelectInput  {...select[0]} />
-                <SelectInput {...select[1]} />
-            </div>
-            <div className="text-left">
-            <SelectInput {...select[2]} />
-            </div> </>:<><div className="text-xl flex gap-1 items-center text-cloud-burst-700"><IconBriefcase className='h-5 w-5 text-cloud-burst-900' stroke={1.5} />{props.role} &bull; {props.company}</div>
-            <div className="text-lg flex gap-1 items-center text-cloud-burst-700">
-                <IconMapPin className='h-5 w-5 text-cloud-burst-900' stroke={1.5} />{props.location}
-            </div></>
-            }
-        </div>
+            <Info />
         <Divider mx="xs" my="xl"/>
         <div className="px-3">
             <div className="text-2xl font-semibold mb-3 flex flex-start text-cloud-burst-700 justify-between">About <ActionIcon onClick={()=>handleEdit(1)} size="lg" color="cloud-burst.9" variant="subtle">
@@ -95,6 +92,7 @@ const Profile=(props:any)=> {
                 }
             </div>
         </div>
+    </div>
     </div>
 }
 
