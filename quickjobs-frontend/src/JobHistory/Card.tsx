87f56c09dd8593708001647008a9a097334e2e66
saurabh-hs <shshinde@mitaoe.ac.in>
2025-07-08 21:34:28 +0530

    jobHistory completed

--- quickjobs-frontend/src/JobHistory/Card.tsx
+++ quickjobs-frontend/src/JobHistory/Card.tsx
@@ -1,22 +1,36 @@
 import { IconBookmark, IconBookmarkFilled, IconCalendarMonth } from '@tabler/icons-react';
-import logo from "../assets/G.png";
 import { Button, Divider, Text } from '@mantine/core';
 import { IconClockHour3 } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
+import { timeAgo } from '../Services/Utilities';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeProfile } from '../Slices/ProfileSlice';
 
 const Card = (props:any)=> {
-    return <Link to="/jobs" className='bg-cloud-burst-400 p-4 w-full flex flex-col gap-3 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !shadow-cloud-burst-900'>
+    const dispatch = useDispatch();
+    const profile = useSelector((state:any)=>state.profile);
+    const handleSaveJob=()=>{
+        let savedJobs:any = [...profile.savedJobs];
+        if(savedJobs?.includes(props.id)) {
+            savedJobs=savedJobs?.filter((id:any)=>id!==props.id);
+        }else{
+            savedJobs=[...savedJobs, props.id];
+        }
+        let updatedProfile={...profile, savedJobs:savedJobs};
+        dispatch(changeProfile(updatedProfile));
+    }
+    return <div className='bg-cloud-burst-400 p-4 w-full flex flex-col gap-3 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !shadow-cloud-burst-900'>
         <div className='flex justify-between'>
             <div className="flex gap-2 items-center">
                 <div className='p-2 bg-cloud-burst-200 rounded-md'>
-                    <img className="h-7" src={logo} alt="compay logo" />
+                    <img className="h-7" src={`../../public/Icons/${props.company}.png`} alt="compay logo" />
                 </div>
                 <div>
                     <div className='text-cloud-burst-50 text-950'>{props.jobTitle}</div>
-                    <div className='text-xs text-cloud-burst-100'>{props.company} &#x2022; {props.applicants} Applicants</div>
+                    <div className='text-xs text-cloud-burst-100'>{props.company} &#x2022; {props.applicants?props.applicants.length:0} Applicants</div>
                 </div>
             </div>
-            {props.saved?<IconBookmarkFilled className='text-cloud-burst-100 cursor-pointer' stroke={2} />:<IconBookmark className='text-cloud-burst-100 cursor-pointer' stroke={2} />}
+            {profile.savedJobs?.includes(props.id)?<IconBookmarkFilled onClick={handleSaveJob} className='cursor-pointer text-cloud-burst-800' stroke={2} />:<IconBookmark onClick={handleSaveJob} className='text-cloud-burst-100 cursor-pointer hover:text-cloud-burst-800' stroke={2} />}
         </div>
         <div className='flex gap-2 [&>div]:py-1 [&>div]:px-2 [&>div]:bg-cloud-burst-600 [&>div]:text-cloud-burst-50 [&>div]:rounded-lg [&>div]:text-xs'>
             <div>{props.experience}</div>
@@ -24,15 +38,15 @@ const Card = (props:any)=> {
             <div>{props.location}</div>
         </div>
         <Text className='!text-xs text-justify !text-cloud-burst-100' lineClamp={2}>
-            {props.description}
+            {props.about}
         </Text>
         <Divider size="xs"/>
         <div className='flex justify-between'>
             <div className='text-cloud-burst-100 font-semibold'>
-                &#8377;{props.package}
+                &#8377;{props.packageOffered}
             </div>
             <div className='flex gap-1 text-xs text-cloud-burst-100 items-center'>
-            <IconClockHour3 className='h-5 w-5 text-cloud-burst-50' stroke={1.5} />{props.applied || props.interviewing?"Applied":props.offered?"Interviewed":"Posted"} {props.postedDaysAgo} days ago
+            <IconClockHour3 className='h-5 w-5 text-cloud-burst-50' stroke={1.5} />{props.applied || props.interviewing?"Applied":props.offered?"Interviewed":"Posted"} {timeAgo(props.postTime)}
             </div>
         </div>
         {(props.offered || props.interviewing)&&<Divider size="xs"/>}
@@ -47,7 +61,10 @@ const Card = (props:any)=> {
                 <IconCalendarMonth className='text-cloud-burst-100 w-5 h-5' stroke={1.5}/> Sun, 25 August &bull; <span className='text-cloud-burst-100'>10:00 AM</span>
             </div>
         }
-    </Link>
+        <Link to={`/jobs/${props.id}`}>
+            <Button fullWidth color='cloud-burst.8' variant='outline'>View Job</Button>
+        </Link>
+    </div>
 }
 
 export default Card;
--- quickjobs-frontend/src/JobHistory/JobHistory.tsx
+++ quickjobs-frontend/src/JobHistory/JobHistory.tsx
@@ -1,47 +1,87 @@
 import { Tabs } from "@mantine/core";
-import { jobList } from "../Data/JobsData";
 import Card from "./Card";
+import { useEffect, useState } from "react";
+import { getAllJobs } from "../Services/JobService";
+import { useSelector } from "react-redux";
 
 const JobHistory=()=> {
+    const [activeTab, setActiveTab] = useState<any>('APPLIED');
+    const user = useSelector((state:any)=>state.user);
+    const [jobList, setJobList] = useState<any>([]);
+    const [showList, setShowList] = useState<any>([]);
+    const profile = useSelector((state:any) => state.profile);
+    useEffect(()=>{
+        getAllJobs().then((res) => {
+            setJobList(res);
+            setShowList(res.filter((job:any)=>{
+                let found = false;
+                job.applicants?.forEach((applicant:any)=>{
+                    if(applicant.applicantId==user.id && applicant.applicationStatus=="APPLIED") {
+                        found = true;
+                    }
+                })
+                return found;
+            }));
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, [])
+
+    const handleTabChange=(value:string | null)=>{
+        setActiveTab(value);
+        if(value=="SAVED") {
+            setShowList(jobList.filter((job:any)=> profile.savedJobs?.includes(job.id)));
+        }else{
+            setShowList(jobList.filter((job:any)=>{
+                let found = false;
+                job.applicants?.forEach((applicant:any)=>{
+                    if(applicant.applicantId==user.id && applicant.applicationStatus==value) {
+                        found = true;
+                    }
+                })
+                return found;
+            }));
+        }
+    }
     return <div className="">
         <div className="text-2xl font-semibold mb-5 text-cloud-burst-900">Job History</div>
         <div>
-            <Tabs variant="outline" radius="lg" defaultValue="about">
+            <Tabs variant="outline" radius="lg" value={activeTab} onChange={handleTabChange}>
             <Tabs.List className="[&_button]:!text-lg font-semibold mb-5 [&_button[data-active='true']]:text-cloud-burst-500 [&_button[data-active='false']]:text-cloud-burst-900">
-            <Tabs.Tab value="applied">Applied</Tabs.Tab>
-            <Tabs.Tab value="saved">Saved</Tabs.Tab>
-            <Tabs.Tab value="offered">Offered</Tabs.Tab>
-            <Tabs.Tab value="interviewing">Interviewing</Tabs.Tab>
+            <Tabs.Tab value="APPLIED">Applied</Tabs.Tab>
+            <Tabs.Tab value="SAVED">Saved</Tabs.Tab>
+            <Tabs.Tab value="OFFERED">Offered</Tabs.Tab>
+            <Tabs.Tab value="INTERVIEWING">Interviewing</Tabs.Tab>
             </Tabs.List>
 
-            <Tabs.Panel value="applied">
+            <Tabs.Panel value={activeTab}>
             <div className="mt-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-full">
                 {
-                    jobList.map((job, index) => <Card key={index} {...job} applied />)
+                    showList.map((job:any, index:any) => <Card key={index} {...job} {...{[activeTab.toLowerCase()]:true}} />)
                 }
             </div>
             </Tabs.Panel>
-            <Tabs.Panel value="saved">
+            {/* <Tabs.Panel value="saved">
             <div className="mt-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-full">
                 {
-                    jobList.map((job, index) => <Card key={index} {...job} saved />)
+                    jobList.map((job:any, index:any) => <Card key={index} {...job} saved />)
                 }
             </div>
             </Tabs.Panel>
             <Tabs.Panel value="offered">
             <div className="mt-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-full">
                 {
-                    jobList.map((job, index) => <Card key={index} {...job} offered />)
+                    jobList.map((job:any, index:any) => <Card key={index} {...job} offered />)
                 }
             </div>
             </Tabs.Panel>
             <Tabs.Panel value="interviewing">
             <div className="mt-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-full">
                 {
-                    jobList.map((job, index) => <Card key={index} {...job} interviewing />)
+                    jobList.map((job:any, index:any) => <Card key={index} {...job} interviewing />)
                 }
             </div>
-            </Tabs.Panel>
+            </Tabs.Panel> */}
             </Tabs>
         </div>
 
