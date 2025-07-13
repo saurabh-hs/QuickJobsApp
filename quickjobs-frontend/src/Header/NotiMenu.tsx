a4db68d90fbb57178665d9e3ced7a17817bd44b1
saurabh-hs <shshinde@mitaoe.ac.in>
2025-07-13 16:21:58 +0530

    Notification completed

--- quickjobs-frontend/src/Header/NotiMenu.tsx
+++ quickjobs-frontend/src/Header/NotiMenu.tsx
@@ -3,8 +3,10 @@ import { IconBell, IconCheck } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getNotifications, readNotifications } from "../Services/NotiService";
+import { useNavigate } from "react-router-dom";
 
 const NotiMenu = () => {
+    const navigate=useNavigate();
     const user = useSelector((state: any) => state.user);
     const [notifications, setNotifications]=useState<any>([]);
     useEffect(()=>{
@@ -22,8 +24,8 @@ const NotiMenu = () => {
     return <Menu shadow="md" width={400} opened={opened} onChange={setOpened}>
         <Menu.Target>
             <div className="bg-cloud-burst-900 p-1.5 rounded-full">
-                <Indicator color="buccaneer.1" offset={6} size={8} processing>
-                    <IconBell stroke={1.5} />
+                <Indicator disabled={notifications.length<=0} color="buccaneer.1" offset={6} size={8} processing>
+                    <IconBell stroke={1.5} />   
                 </Indicator>
             </div>
         </Menu.Target>
@@ -31,7 +33,11 @@ const NotiMenu = () => {
         <Menu.Dropdown onChange={() => setOpened(true)}>
             <div className="flex flex-col gap-1">
                 {
-                    notifications.map((noti:any, index:number)=> <Notification key={index} onClose={()=>unread(index)} className="hover:bg-cloud-burst-900 cursor-pointer" icon={<IconCheck style={{width: rem(20), height: rem(20) }}/>} color="teal" title={noti.action} mt="md">{noti.message}</Notification>)
+                    notifications.map((noti:any, index:number)=> <Notification onClick={()=>{
+                        navigate(noti.route);
+                        setOpened(false);
+                        unread(index);
+                    }} key={index} onClose={()=>unread(index)} className="hover:bg-cloud-burst-900 cursor-pointer" icon={<IconCheck style={{width: rem(20), height: rem(20) }}/>} color="teal" title={noti.action} mt="md">{noti.message}</Notification>)
                 }
                 {
                     notifications.length==0 && <div className="text-center text-cloud-burst-600">No Notifications</div>
